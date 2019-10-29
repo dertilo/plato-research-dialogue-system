c94fa99a5897861772d78b85ff42222c3e2d30a8
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-29 15:07:00 +0100

    use simplified agent

--- run_plato_simplified.py
+++ run_plato_simplified.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 from tqdm import tqdm
 
-from ConversationalAgent.ConversationalSingleAgent import ConversationalSingleAgent
+from ConversationalAgent.ConversationalSingleAgentSimplified import ConversationalSingleAgent
 
 import yaml
 import os.path
