d3c6608232407b22220059df5004e14e62e27df2
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-29 15:05:04 +0100

    cleaning

--- run_plato_simplified.py
+++ run_plato_simplified.py
@@ -1,35 +1,14 @@
-"""
-Copyright (c) 2019 Uber Technologies, Inc.
-
-Licensed under the Uber Non-Commercial License (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at the root directory of this project. 
-
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 from pprint import pprint
 from tqdm import tqdm
 
 from ConversationalAgent.ConversationalSingleAgent import ConversationalSingleAgent
 
-__author__ = "Alexandros Papangelis"
-
-import configparser
 import yaml
-import sys
 import os.path
 import time
 import random
 
 def run_single_agent(config, num_dialogues):
-    """
-    This function will create an agent and orchestrate the conversation.
-
-    :param config: a dictionary containing settings
-    :param num_dialogues: how many dialogues to run for
-    :return: some statistics
-    """
     ca = ConversationalSingleAgent(config)
     ca.initialize()
 
