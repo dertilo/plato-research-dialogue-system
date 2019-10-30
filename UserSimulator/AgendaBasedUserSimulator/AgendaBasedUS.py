c56f9f9ac2138b9559f34c3d04d1bcc551b1b5d1
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-30 20:04:52 +0100

    no trained Simulator in AgendaBasedUS; no abstract UserSimulator-Class

--- UserSimulator/AgendaBasedUserSimulator/AgendaBasedUS.py
+++ UserSimulator/AgendaBasedUserSimulator/AgendaBasedUS.py
@@ -1,13 +1,9 @@
-import os
-import pickle
 import random
 from copy import deepcopy
+from typing import List
 
 from Dialogue.Action import DialogueAct, DialogueActItem, Operator
-from Domain.DataBase import DataBase, SQLDataBase, JSONDataBase
-from Domain.Ontology import Ontology
 from UserSimulator.AgendaBasedUserSimulator import Agenda, Goal, ErrorModel
-from .. import UserSimulator
 
 """
 AgendaBasedUS is Plato's implementation of the Agenda-Based Simulated Usr, as 
@@ -19,11 +15,11 @@ Proc SIGDial, Antwerp 273282.9 (2007).
 """
 
 
-class AgendaBasedUS(UserSimulator.UserSimulator):
+class AgendaBasedUS(object):
     def __init__(
         self,
-        goal_generator:Goal.GoalGenerator,
-        error_model:ErrorModel,
+        goal_generator: Goal.GoalGenerator,
+        error_model: ErrorModel,
         patience=3,
         pop_distribution=[1.0],
         goal_slot_selection_weights=None,
@@ -32,7 +28,6 @@ class AgendaBasedUS(UserSimulator.UserSimulator):
         super(AgendaBasedUS, self).__init__()
 
         self.dialogue_turn = 0
-        self.policy = None
         self.patience = patience
         self.pop_distribution = pop_distribution
 
@@ -67,128 +62,35 @@ class AgendaBasedUS(UserSimulator.UserSimulator):
         self.offer_made = False
         self.prev_offer_name = None
 
-    def receive_input(self, inpt, goal=None):
-        """
-        Receives and processes the input (i.e. the system's response) and
-        updates the simulator's internal state.
-
-        :param inpt: list of dialogue acts representing input from the system
-        :param goal: the simulated user's goal
-        :return: Nothing
-        """
-
-        system_acts = inpt
-
-        if goal:
-            self.goal = goal
+    def receive_input(self, system_acts: List[DialogueAct]):
 
         self.dialogue_turn += 1
-
-        # TODO: AVOID RE-CHECKING THIS IN THE HANDCRAFTED POLICY
-
-        # Update user goal (in ABUS the state is factored into the goal and
-        # the agenda)
         for system_act in system_acts:
             if system_act.intent == "offer":
-                self.offer_made = True
+                self.handle_offer(system_act)
 
-                # Reset past requests
-                if (
-                    self.prev_offer_name
-                    and system_act.params
-                    and system_act.params[0].slot
-                    and system_act.params[0].slot == "name"
-                    and system_act.params[0].value
-                    and self.prev_offer_name != system_act.params[0].value
-                ):
-
-                    self.prev_offer_name = system_act.params[0].value
-
-                    self.goal.requests_made = {}
-
-                    for item in self.goal.requests:
-                        item.value = ""
-
-        if self.policy:
-            self.receive_input_policy(system_acts)
-        else:
-            self.receive_input_handcrafted(system_acts)
+        self.receive_input_handcrafted(system_acts)
 
         self.agenda.consistency_check()
 
-    def receive_input_policy(self, system_acts):
-        """
-        Handle the input according to a policy
+    def handle_offer(self, system_act):
+        self.offer_made = True
+        # Reset past requests
+        if (
+            self.prev_offer_name
+            and system_act.params
+            and system_act.params[0].slot
+            and system_act.params[0].slot == "name"
+            and system_act.params[0].value
+            and self.prev_offer_name != system_act.params[0].value
+        ):
 
-        :param system_acts: a list with the system's dialogue acts
-        :return: Nothing
-        """
+            self.prev_offer_name = system_act.params[0].value
 
-        if self.prev_system_acts and self.prev_system_acts == system_acts:
-            self.curr_patience -= 1
-        else:
-            self.curr_patience = self.patience
+            self.goal.requests_made = {}
 
-        self.prev_system_acts = deepcopy(system_acts)
-
-        for system_act in system_acts:
-            # 'bye' doesn't seem to appear in the CamRest data
-            if (
-                system_act.intent == "bye"
-                or self.curr_patience == 0
-                or self.dialogue_turn > 15
-            ):
-                self.agenda.push(DialogueAct("bye", []))
-                return
-
-            sys_act_slot = (
-                "inform" if system_act.intent == "offer" else system_act.intent
-            )
-
-            if system_act.params and system_act.params[0].slot:
-                sys_act_slot += "_" + system_act.params[0].slot
-
-            # Attempt to recover
-            if sys_act_slot not in self.policy:
-                if sys_act_slot == "inform_name":
-                    sys_act_slot = "offer_name"
-
-            if sys_act_slot not in self.policy:
-                if (
-                    system_act.intent == "inform"
-                    and system_act.params
-                    and system_act.params[0].slot in self.goal.constraints
-                ):
-                    user_act_slots = ["inform_" + system_act.params[0].slot]
-                else:
-                    print(
-                        "Warning! ABUS policy does not know what to do for"
-                        " %s" % sys_act_slot
-                    )
-                    return
-            else:
-                dacts = list(self.policy[sys_act_slot]["dacts"].keys())
-                probs = [self.policy[sys_act_slot]["dacts"][i] for i in dacts]
-
-                user_act_slots = random.choices(dacts, weights=probs)
-
-            for user_act_slot in user_act_slots:
-                intent, slot = user_act_slot.split("_")
-
-                if slot == "this" and system_act.params and system_act.params[0].slot:
-                    slot = system_act.params[0].slot
-
-                value = ""
-                if intent == "inform":
-                    if slot in self.goal.constraints:
-                        value = self.goal.constraints[slot].value
-                    else:
-                        value = "dontcare"
-
-                dact = DialogueAct(intent, [DialogueActItem(slot, Operator.EQ, value)])
-
-                self.agenda.remove(dact)
-                self.agenda.push(dact)
+            for item in self.goal.requests:
+                item.value = ""
 
     def receive_input_handcrafted(self, system_acts):
         """
@@ -318,11 +220,6 @@ class AgendaBasedUS(UserSimulator.UserSimulator):
             # TODO Relax goals if system returns no info for name
 
     def respond(self):
-        """
-        Creates the response of the simulated user.
-
-        :return: List of DialogueActs as a response
-        """
 
         if self.curr_patience == 0:
             return [DialogueAct("bye", [])]
@@ -347,52 +244,3 @@ class AgendaBasedUS(UserSimulator.UserSimulator):
             acts.append(act)
 
         return acts
-
-    def train(self, data):
-        """
-        Placeholder for training models
-
-        :param data: dialogue experience
-        :return: nothing
-        """
-        pass
-
-    def save(self, path=None):
-        """
-        Placeholder for saving trained models
-
-        :param path: path to save the models
-        :return: nothing
-        """
-        pass
-
-    def load(self, path):
-        """
-        Load an Agenda-Based Simulated Usr policy
-
-        :param path: path to the policy file
-        :return: nothing
-        """
-
-        if isinstance(path, str):
-            if os.path.isfile(path):
-                with open(path, "rb") as file:
-                    obj = pickle.load(file)
-
-                    if "policy" in obj:
-                        self.policy = obj["policy"]
-
-                    print("ABUS policy loaded.")
-
-            else:
-                raise FileNotFoundError("ABUS policy file %s not found" % path)
-        else:
-            raise ValueError("Unacceptable ABUS policy file name: %s " % path)
-
-    def at_terminal_state(self):
-        """
-        Check if the Agenda-Based Simulated Usr is at a terminal state
-        :return: True or False
-        """
-
-        return not self.agenda.agenda
