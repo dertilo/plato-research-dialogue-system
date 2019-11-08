6ac47bc2319d2270499dcefa9ba806849147c264
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-11-08 17:19:21 +0100

    refactor Action

--- Dialogue/Action.py
+++ Dialogue/Action.py
@@ -8,16 +8,34 @@ You may obtain a copy of the License at the root directory of this project.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from dataclasses import dataclass
+from typing import List, Any
 
 __author__ = "Alexandros Papangelis"
 
 from enum import Enum
 
-"""
-The Action class models actions that a Conversational Agent or Simulated Usr 
-can take. It defines the interface that any other act (e.g. dialogue act, 
-multi-modal act, etc.) should follow.
-"""
+
+class Operator(Enum):
+    EQ = 1
+    NE = 2
+    LT = 3
+    LE = 4
+    GT = 5
+    GE = 6
+
+    AND = 7
+    OR = 8
+    NOT = 9
+    IN = 10
+
+    def __str__(self):
+        """
+        Print the Operator
+
+        :return: a string representation of the Operator
+        """
+        return f"{self.name}"
 
 
 class Action:
@@ -44,91 +62,13 @@ class SummaryAction(Enum):
     REQUEST_X = 9
     NOTHING = 10
 
-
-""" 
-The DialogueAct models dialogue acts, each of which has an intent and a list 
-of parameters.
-"""
-
-
-class DialogueAct(Action):
-    """
-    Represents a dialogue act, which as a type (e.g. inform, request, etc.)
-    and a list of DialogueActItem parameters, which are triplets of
-    <slot, operator, value>.
-    """
-    def __init__(self, intent:str, params=[]):
-        super(DialogueAct, self).__init__()
-
-        self.name = 'dialogue_act'
-        self.intent = intent
-        self.params = params
-
-    def __eq__(self, other):
-        """
-        Equality operator.
-
-        :param other: the Dialogue Act to compare against
-        :return: True of False
-        """
-
-        # TODO: Make the check more efficient
-        return self.funcName == other.funcName and \
-            self.intent == other.intent and \
-            self.name == other.name and \
-            [s for s in self.params if s not in other.params] == []
-
-    def __str__(self):
-        """
-        Pretty print Dialogue Act.
-
-        :return: string representation of the Dialogue Act
-        """
-
-        if self.intent:
-            return self.intent + \
-                   '(' + \
-                   ''.join([str(param)+', ' for param in self.params]) + ')'
-        else:
-            return 'None (DialogueAct)'
-
-
-"""
-The DialogueActItem models a parameter of a DialogueAct. It is essentially a 
-triplet of (slot, operator, value).
-"""
-
-
+@dataclass
 class DialogueActItem:
-    def __init__(self, slot, op, value):
-        """
-        Initialize a Dialogue Act Item (slot - operator - value)
-
-        :param slot: a string, representing the slot
-        :param op: an Operator
-        :param value: the value of the slot
-        """
-        if isinstance(slot, str):
-            self.slot = slot
-        else:
-            raise ValueError('Unacceptable slot type: %s ' % slot)
-
-        if op in Operator:
-            self.op = op
-        else:
-            raise ValueError('Unacceptable operator: %s ' % op)
-
-        # TODO: Check for all acceptable value types here?
-        self.value = value
+    slot:str
+    op:Operator
+    value:Any
 
     def __eq__(self, other):
-        """
-        Equality operator
-
-        :param other: the Dialogue Act Item to compare against
-        :return: True or False
-        """
-
         # TODO: Will need some kind of constraint satisfaction (with tolerance)
         # to efficiently handle all operators
         return self.slot == other.slot and self.op == other.op and \
@@ -171,6 +111,38 @@ class DialogueActItem:
         return result
 
 
+class DialogueAct(Action):
+    """
+    Represents a dialogue act, which as a type (e.g. inform, request, etc.)
+    and a list of DialogueActItem parameters, which are triplets of
+    <slot, operator, value>.
+    """
+    def __init__(self, intent:str, params:List[DialogueActItem]=[]):
+        super(DialogueAct, self).__init__()
+
+        self.name = 'dialogue_act'
+        self.intent = intent
+        self.params:List[DialogueActItem] = params
+
+    def __eq__(self, other):
+
+        # TODO: Make the check more efficient
+        return self.funcName == other.funcName and \
+            self.intent == other.intent and \
+            self.name == other.name and \
+            [s for s in self.params if s not in other.params] == []
+
+    def __str__(self):
+
+        if self.intent:
+            return self.intent + \
+                   '(' + \
+                   ''.join([str(param)+', ' for param in self.params]) + ')'
+        else:
+            return 'None (DialogueAct)'
+
+
+
 """
 The Expression class models complex expressions and defines how to compute 
 them.
@@ -187,32 +159,6 @@ class Expression:
         """
         pass
 
-
-"""
-The Operator class defines acceptable operators.
-"""
-
-
-class Operator(Enum):
-    EQ = 1
-    NE = 2
-    LT = 3
-    LE = 4
-    GT = 5
-    GE = 6
-
-    AND = 7
-    OR = 8
-    NOT = 9
-    IN = 10
-
-    def __str__(self):
-        """
-        Print the Operator
-
-        :return: a string representation of the Operator
-        """
-        return f"{self.name}"
 
 
 # Represents an event of the (simulated) user tapping onto something in the
