841476959046061ede03846c5b6ed131eca8e9ac
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-29 09:54:51 +0200

    if temp.user_acts is not None

--- DialogueManagement/DialoguePolicy/dialogue_common.py
+++ DialogueManagement/DialoguePolicy/dialogue_common.py
@@ -103,6 +103,7 @@ def state_to_json(state:SlotFillingDialogueState)->str:
     temp.slots_filled = [s for s,v in temp.slots_filled.items() if v is not None]
     if temp.last_sys_acts is not None:
         temp.last_sys_acts = action_to_string(temp.last_sys_acts, system=True)
+    if temp.user_acts is not None:
         temp.user_acts = action_to_string(temp.user_acts, system=False)
 
     d = todict(temp)
