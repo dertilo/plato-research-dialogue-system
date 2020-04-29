f9a060886e62329871904dce464c68299371537f
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-29 10:32:43 +0200

    refactor RewardFunction.py

--- DialogueManagement/DialoguePolicy/ReinforcementLearning/RewardFunction.py
+++ DialogueManagement/DialoguePolicy/ReinforcementLearning/RewardFunction.py
@@ -21,7 +21,6 @@ reinforcement learning.
 
 
 class Reward(ABC):
-
     @abstractmethod
     def initialize(self, **kwargs):
         """
@@ -64,15 +63,16 @@ class SlotFillingReward(Reward):
         :return: Nothing
         """
 
-        if 'turn_penalty' in kwargs:
-            self.turn_penalty = kwargs['turn_penalty']
-        if 'failure_penalty' in kwargs:
-            self.failure_penalty = kwargs['failure_penalty']
-        if 'success_reward' in kwargs:
-            self.success_reward = kwargs['success_reward']
+        if "turn_penalty" in kwargs:
+            self.turn_penalty = kwargs["turn_penalty"]
+        if "failure_penalty" in kwargs:
+            self.failure_penalty = kwargs["failure_penalty"]
+        if "success_reward" in kwargs:
+            self.success_reward = kwargs["success_reward"]
 
-    def calculate(self, state, actions, goal=None, force_terminal=False,
-                  agent_role='system'):
+    def calculate(
+        self, state, actions, goal=None, force_terminal=False, agent_role="system"
+    ):
         """
         Calculate the reward to be assigned for taking action from state.
 
@@ -88,7 +88,7 @@ class SlotFillingReward(Reward):
         reward = self.turn_penalty
 
         if goal is None:
-            print('Warning: SlotFillingReward() called without a goal.')
+            print("Warning: SlotFillingReward() called without a goal.")
             return 0, False, False
 
         else:
@@ -97,61 +97,23 @@ class SlotFillingReward(Reward):
             if state.is_terminal() or force_terminal:
                 # Check that an offer has actually been made
                 if state.system_made_offer:
-                    dialogue_success = True
-
-                    # Check that the item offered meets the user's constraints
-                    for constr in goal.constraints:
-                        if goal.ground_truth:
-                            # Multi-agent case
-                            if goal.ground_truth[constr] != \
-                                    goal.constraints[constr].value and \
-                                    goal.constraints[constr].value != \
-                                    'dontcare':
-                                reward += self.failure_penalty
-                                dialogue_success = False
-                                break
-
-                        elif state.item_in_focus:
-                            # Single-agent case
-                            if state.item_in_focus[constr] != \
-                                    goal.constraints[constr].value and \
-                                    goal.constraints[constr].value != \
-                                    'dontcare':
-                                reward += self.failure_penalty
-                                dialogue_success = False
-                                break
-
-                    # Check that all requests have been addressed
-                    if dialogue_success:
-                        not_met = 0
-
-                        if agent_role == 'system':
-                            # Check that the system has responded to all
-                            # requests (actually) made by the user
-                            for req in goal.actual_requests:
-                                if not goal.actual_requests[req].value:
-                                    not_met += 1
-
-                        elif agent_role == 'user':
-                            # Check that the user has provided all the
-                            # requests in the goal
-                            for req in goal.requests:
-                                if not goal.requests[req].value:
-                                    not_met += 1
-
-                        if not_met > 0:
-                            reward += self.failure_penalty
-                            dialogue_success = False
-                        else:
-                            reward = self.success_reward
+                    dialogue_success, reward = self._evaluate_offer(
+                        agent_role, goal, reward, state
+                    )
 
                 else:
                     reward += self.failure_penalty
                     dialogue_success = False
 
-        # Liu & Lane ASRU 2017 Definition of task success
+        # # Liu & Lane ASRU 2017 Definition of task success
+        # task_success = self.calc_task_success_after_Liu_and_Lane(agent_role, goal,
+        #                                                          state)
+        task_success = None  # Tilo: we are not using this!
+        return reward, dialogue_success, task_success
+
+    def calc_task_success_after_Liu_and_Lane(self, agent_role, goal, state):
         task_success = None
-        if agent_role == 'system':
+        if agent_role == "system":
             task_success = True
             # We don't care for slots that are not in the goal constraints
             for slot in goal.constraints:
@@ -160,17 +122,19 @@ class SlotFillingReward(Reward):
                 # the user's DState is updated accordingly and the user would
                 # not need to put that constraint.
                 if goal.ground_truth:
-                    if goal.ground_truth[slot] != \
-                            goal.constraints[slot].value and \
-                            goal.constraints[slot].value != 'dontcare':
+                    if (
+                        goal.ground_truth[slot] != goal.constraints[slot].value
+                        and goal.constraints[slot].value != "dontcare"
+                    ):
                         task_success = False
                         break
 
                 # Fall back to the noisier signal, that is the slots filled.
-                elif slot in state.slots_filled and \
-                        state.slots_filled[slot] != \
-                        goal.constraints[slot].value and \
-                        goal.constraints[slot].value != 'dontcare':
+                elif (
+                    slot in state.slots_filled
+                    and state.slots_filled[slot] != goal.constraints[slot].value
+                    and goal.constraints[slot].value != "dontcare"
+                ):
                     task_success = False
                     break
 
@@ -178,8 +142,55 @@ class SlotFillingReward(Reward):
                 if not goal.requests[req].value:
                     task_success = False
                     break
+        return task_success
 
-        return reward, dialogue_success, task_success
+    def _evaluate_offer(self, agent_role, goal, reward, state):
+        dialogue_success = True
+        # Check that the item offered meets the user's constraints
+        for constr in goal.constraints:
+            if goal.ground_truth:
+                # Multi-agent case
+                if (
+                    goal.ground_truth[constr] != goal.constraints[constr].value
+                    and goal.constraints[constr].value != "dontcare"
+                ):
+                    reward += self.failure_penalty
+                    dialogue_success = False
+                    break
+
+            elif state.item_in_focus:
+                # Single-agent case
+                if (
+                    state.item_in_focus[constr] != goal.constraints[constr].value
+                    and goal.constraints[constr].value != "dontcare"
+                ):
+                    reward += self.failure_penalty
+                    dialogue_success = False
+                    break
+        # Check that all requests have been addressed
+        if dialogue_success:
+            not_met = 0
+
+            if agent_role == "system":
+                # Check that the system has responded to all
+                # requests (actually) made by the user
+                for req in goal.actual_requests:
+                    if not goal.actual_requests[req].value:
+                        not_met += 1
+
+            elif agent_role == "user":
+                # Check that the user has provided all the
+                # requests in the goal
+                for req in goal.requests:
+                    if not goal.requests[req].value:
+                        not_met += 1
+
+            if not_met > 0:
+                reward += self.failure_penalty
+                dialogue_success = False
+            else:
+                reward = self.success_reward
+        return dialogue_success, reward
 
 
 class SlotFillingGoalAdvancementReward(Reward):
@@ -201,21 +212,22 @@ class SlotFillingGoalAdvancementReward(Reward):
         :return: Nothing
         """
 
-        if 'failure_penalty' in kwargs:
-            self.failure_penalty = kwargs['failure_penalty']
-        if 'success_reward' in kwargs:
-            self.success_reward = kwargs['success_reward']
-        if 'state' in kwargs:
-            self.prev_state = deepcopy(kwargs['state'])
+        if "failure_penalty" in kwargs:
+            self.failure_penalty = kwargs["failure_penalty"]
+        if "success_reward" in kwargs:
+            self.success_reward = kwargs["success_reward"]
+        if "state" in kwargs:
+            self.prev_state = deepcopy(kwargs["state"])
         else:
             self.prev_state = None
-        if 'goal' in kwargs:
-            self.prev_goal = deepcopy(kwargs['goal'])
+        if "goal" in kwargs:
+            self.prev_goal = deepcopy(kwargs["goal"])
         else:
             self.prev_goal = None
 
-    def calculate(self, state, actions, goal=None, force_terminal=False,
-                  agent_role='system'):
+    def calculate(
+        self, state, actions, goal=None, force_terminal=False, agent_role="system"
+    ):
         """
         Calculate the reward based on whether the action taken advanced the
         goal or not. For example, if the action resulted in filling one more
@@ -230,8 +242,9 @@ class SlotFillingGoalAdvancementReward(Reward):
         """
 
         if goal is None:
-            print('Warning: SlotFillingGoalAdvancementReward() called without '
-                  'a goal.')
+            print(
+                "Warning: SlotFillingGoalAdvancementReward() called without " "a goal."
+            )
             return -1, False, False
 
         elif self.prev_state is None or self.prev_goal is None:
@@ -240,21 +253,27 @@ class SlotFillingGoalAdvancementReward(Reward):
         # Check if the goal has been advanced
         else:
             # If the new state has more slots filled than the old one
-            if sum([1 if self.prev_state.slots_filled[s] else 0 for s in
-                    self.prev_state.slots_filled]) < \
-                    sum([1 if state.slots_filled[s] else 0 for s in
-                         state.slots_filled]):
+            if sum(
+                [
+                    1 if self.prev_state.slots_filled[s] else 0
+                    for s in self.prev_state.slots_filled
+                ]
+            ) < sum([1 if state.slots_filled[s] else 0 for s in state.slots_filled]):
                 reward = 1
 
             # Or if the new state has more requests filled than the old one
-            elif sum([1 if self.prev_goal.actual_requests[r] else 0 for r in
-                      self.prev_goal.actual_requests]) < \
-                    sum([1 if goal.actual_requests[r] else 0 for r in
-                         goal.actual_requests]):
+            elif sum(
+                [
+                    1 if self.prev_goal.actual_requests[r] else 0
+                    for r in self.prev_goal.actual_requests
+                ]
+            ) < sum(
+                [1 if goal.actual_requests[r] else 0 for r in goal.actual_requests]
+            ):
                 reward = 1
 
             # Or if the system made a request for an unfilled slot?
-        
+
             else:
                 reward = -1
 
@@ -268,15 +287,16 @@ class SlotFillingGoalAdvancementReward(Reward):
                 for constr in goal.constraints:
                     if goal.ground_truth:
                         # Multi-agent case
-                        if goal.ground_truth[constr] != \
-                                goal.constraints[constr].value:
+                        if goal.ground_truth[constr] != goal.constraints[constr].value:
                             success = False
                             break
 
                     elif state.item_in_focus:
                         # Single-agent case
-                        if state.item_in_focus[constr] != \
-                                goal.constraints[constr].value:
+                        if (
+                            state.item_in_focus[constr]
+                            != goal.constraints[constr].value
+                        ):
                             success = False
                             break
 
@@ -284,7 +304,7 @@ class SlotFillingGoalAdvancementReward(Reward):
         self.prev_goal = deepcopy(goal)
 
         task_success = None
-        if agent_role == 'system':
+        if agent_role == "system":
             task_success = True
             # We don't care for slots that are not in the goal constraints
             for slot in goal.constraints:
@@ -293,15 +313,17 @@ class SlotFillingGoalAdvancementReward(Reward):
                 # the user's DState is updated accordingly and the user would
                 # not need to put that constraint.
                 if goal.ground_truth:
-                    if goal.ground_truth[slot] != \
-                            goal.constraints[slot].value and \
-                            goal.constraints[slot].value != 'dontcare':
+                    if (
+                        goal.ground_truth[slot] != goal.constraints[slot].value
+                        and goal.constraints[slot].value != "dontcare"
+                    ):
                         task_success = False
 
                 # Fall back to the noisier signal, that is the slots filled.
-                elif state.slots_filled[slot] != \
-                        goal.constraints[slot].value and \
-                        goal.constraints[slot].value != 'dontcare':
+                elif (
+                    state.slots_filled[slot] != goal.constraints[slot].value
+                    and goal.constraints[slot].value != "dontcare"
+                ):
                     task_success = False
 
             for req in goal.requests:
