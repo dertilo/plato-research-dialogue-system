331b5e23310f856f779bc2fd77109901d1601060
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-13 09:02:46 +0200

    plot_results.py

--- plot_results.py
+++ plot_results.py
@@ -8,7 +8,7 @@ from util import data_io
 from matplotlib import pyplot as plt
 
 
-def plot_results(scoring_runs: List[Dict], figure_file="results.png"):
+def plot_results(scoring_runs: List[Dict], save_path):
     sns.set(style="ticks", palette="pastel")
     data = [
         {
@@ -26,19 +26,22 @@ def plot_results(scoring_runs: List[Dict], figure_file="results.png"):
         lambda d: d['exp_name'].startswith('pytorch_a2c'),
         lambda d: d['exp_name'].endswith('error_sim'),
         lambda d: d['exp_name'].endswith('two_slots'),
+        lambda d: True,
     ]
     for filter_fun in filter_funs:
         df = pd.DataFrame(data=list(filter(filter_fun,data)))
+        if df.size==0:
+            continue
         fig, ax = plt.subplots(figsize=(30, 10))
         ax = sns.boxplot(
             ax=ax,
             x="exp_name", y="success-rate", hue="split-name", palette=["m", "g", "r"], data=df
         )
         # sns.despine(offset=10, trim=True)
-        ax.set_title("%d runs, 2000 train and 1000 eval dialogues" % (num_runs))
+        ax.set_title("%d runs, 5000 train and 1000 eval dialogues, 500 warmup" % (num_runs))
         # ax.set_xlabel("")
         filter_fun_str = inspect.getsourcelines(filter_fun)[0][0].split(':')[1].strip('\n').strip(',')
-        ax.figure.savefig(figure_file.split('.')[0]+"_%s.png"%filter_fun_str)
+        ax.figure.savefig(save_path+"/boxplots_%s.png"%filter_fun_str)
 
     # plt.show()
     plt.close()
@@ -52,6 +55,8 @@ def get_num_runs(data):
 
 
 if __name__ == "__main__":
-    scoring_runs = list(data_io.read_jsonl(os.environ["HOME"]+"/gunther/DIALOGUE/plato/scores.jsonl"))
+    path = os.environ["HOME"] + "/gunther/interspeech_2020/data/5000_500"
+    # file = "scores_2000traindialogues.jsonl"
+    scoring_runs = list(data_io.read_jsonl(path + '/scores.jsonl'))
 
-    plot_results(scoring_runs)
+    plot_results(scoring_runs,path)
